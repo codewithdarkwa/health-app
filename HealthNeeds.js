f08814e55e145265954d58f5e5fab9b8b305f3ae
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-30 20:44:25 +0000

    added a review

--- HealthNeeds.js
+++ HealthNeeds.js
@@ -7,19 +7,19 @@ export default function(){
 <View style={{flexDirection:"row",justifyContent:"space-around",}}>
     <TouchableOpacity style={{flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
         <Health />
-        <Text style={{alignSelf:"center"}}>Appointment</Text>
+        <Text style={{alignSelf:"center",color:"gray"}}>Appointment</Text>
      </TouchableOpacity>
      <TouchableOpacity style={{flexDirection:"column",alignItems:"center"}}>
         <House />
-        <Text style={{alignSelf:"center"}}>Hospital</Text>
+        <Text style={{alignSelf:"center",color:"gray"}}>Hospital</Text>
      </TouchableOpacity>
      <TouchableOpacity style={{flexDirection:"column",alignItems:"center"}}>
         <VirusIcon />
-        <Text style={{alignSelf:"center"}}>Covid 19</Text>
+        <Text style={{alignSelf:"center",color:"gray"}}>Covid 19</Text>
     </TouchableOpacity>
      <TouchableOpacity style={{flexDirection:"column",alignItems:"center"}}>
         <Apps />
-        <Text style={{alignSelf:"center"}}>More</Text>
+        <Text style={{alignSelf:"center",color:"gray"}}>More</Text>
      </TouchableOpacity>
     </View>
     )
